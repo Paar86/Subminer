1cbcfe0f46b88b5225efc90c2944feb984937752
Paar86 <josu@seznam.cz>
2022-07-16 21:49:22 +0200

    Animated title screen

--- src/UI/TitlePage.gd
+++ src/UI/TitlePage.gd
@@ -5,6 +5,8 @@ signal new_game_pressed
 
 onready var NewGameButton = $VBoxContainer/Buttons/NewGameButton
 onready var HowToButton = $VBoxContainer/Buttons/HowToButton
+onready var ButtonENG = $VBoxContainer/Buttons/LanguageButtons/ButtonENG
+onready var ButtonCZE = $VBoxContainer/Buttons/LanguageButtons/ButtonCZE
 onready var AnimationPlayerScene = $AnimationPlayer
 
 onready var _screen_size := get_viewport_rect().size
@@ -12,23 +14,30 @@ onready var _screen_size := get_viewport_rect().size
 
 func _ready() -> void:
 	_refresh_text()
-	_generate_bubble()
+	AnimationPlayerScene.play("INTRO")
 
 
 func _refresh_text() -> void:
 	NewGameButton.text = TextManager.get_string_by_key("new_game_title")
 	HowToButton.text = TextManager.get_string_by_key("how_to_title")
-	
-	
+
+
 func _generate_bubble() -> void:
 	var y = _screen_size.y + 20.0
 	var x = rand_range(0.0, _screen_size.x)
-	
+
 	BubbleGenerator.generate_bubble_to_position(Vector2(x, y), self)
 
 
+func _activate_buttons():
+	NewGameButton.disabled = false
+	HowToButton.disabled = false
+	ButtonENG.disabled = false
+	ButtonCZE.disabled = false
+
+
 func _on_NewGameButton_pressed() -> void:
-	AnimationPlayerScene.play("fade_out")
+	AnimationPlayerScene.play("FADE_OUT")
 	yield(AnimationPlayerScene, "animation_finished")
 	emit_signal("new_game_pressed")
 
@@ -49,3 +58,9 @@ func _on_ButtonCZE_toggled(button_pressed: bool) -> void:
 
 func _on_BubbleTimer_timeout() -> void:
 	_generate_bubble()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "INTRO":
+		_generate_bubble()
+		$BubbleTimer.start()
